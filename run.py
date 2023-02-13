341773da187f21bbb13b1c1e3fd9d5b74991c880
BlueBindy <fionajthompson@outlook.com>
2023-02-13 11:28:17 +0000

    Add function to get last 5 sales data

--- run.py
+++ run.py
@@ -87,6 +87,20 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
 def update_surplus_worksheet(data):
     """
     Update surplus worksheet, add new row with the surplus data provided
@@ -118,7 +132,10 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(f"surplus data: {new_surplus_data}")
     update_worksheet(new_surplus_data, 'surplus')
+    
 
 
 print('Welcome to Love Sandwiches data automation')
 main()
+
+sales_columns = get_last_5_entries_sales()
